4eb5378172b34dbfbd55328f9c98414b5ea326f6
mageuss <mateus.santos@aedb.br>
2022-09-28 20:41:56 -0300

    fourth commit

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,17 @@
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
+  
+  exports:[
+    HeaderComponent
+  ],
+  
   imports: [
     CommonModule
   ]
